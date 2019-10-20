b361ae0da93af1b4665fb0269d5c53b7584a5306
vueEasy <603850549@qq.com>
2019-10-20 10:44:43 +0800

    develop ment

--- src/store/aabc.js
+++ src/store/aabc.js
@@ -1,6 +1,6 @@
 export let acx=(state = 1,action)=>{
     if(action.type === "1213"){
-        return "asdasdasd111,develop develop"
+        return "asdasdasd111，develop"
     }else{
         return state
     }
